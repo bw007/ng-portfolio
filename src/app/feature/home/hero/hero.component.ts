8865103ceb1e6b7478088763240755d6fd92d8c3
bw007 <mr.khasanov02@gmail.com>
2026-04-21 23:41:49 +0500

    feat: implement text animation directive

--- src/app/feature/home/hero/hero.component.ts
+++ src/app/feature/home/hero/hero.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { ScrambleTextDirective } from "@app/shared/directives/scramble-text.directive";
 
 @Component({
   selector: 'app-hero',
-  imports: [],
+  imports: [ScrambleTextDirective],
   templateUrl: './hero.component.html',
 })
 export class HeroComponent {}

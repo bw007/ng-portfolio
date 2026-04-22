7408cf47669c1735853ab1261514939d35a9c5d3
bw007 <mr.khasanov02@gmail.com>
2026-04-22 23:58:41 +0500

    feat: add scramble text directive with gsap animation

--- src/app/shared/directives/scramble-text.directive.ts
+++ src/app/shared/directives/scramble-text.directive.ts
@@ -1,29 +1,39 @@
 import { isPlatformBrowser } from "@angular/common";
-import { AfterViewInit, Directive, ElementRef, inject, PLATFORM_ID } from "@angular/core";
+import { AfterViewInit, Directive, ElementRef, inject, input, PLATFORM_ID } from "@angular/core";
 import { gsap } from "gsap";
 import { ScrambleTextPlugin } from "gsap/ScrambleTextPlugin";
 
-gsap.registerPlugin(ScrambleTextPlugin) 
+gsap.registerPlugin(ScrambleTextPlugin);
 
 @Directive({
-  selector: '[scrambleText]',
+  selector: 'span[scrambleText]',
 })
 export class ScrambleTextDirective implements AfterViewInit {
-  readonly el = inject(ElementRef);
+  readonly text = input<string>('{original}');
+  readonly chars = input<string>('upperCase');
+  readonly duration = input<number>(2);
+  readonly delay = input<number>(0);
+  readonly revealDelay = input<number>(0);
+  readonly speed = input<number>(1);
+  readonly delimiter = input<string>('');
+
+  private readonly el = inject(ElementRef);
   private readonly platformId = inject(PLATFORM_ID);
 
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
       gsap.to(this.el.nativeElement, {
+        duration: this.duration(),
+        delay: this.delay(),
         scrambleText: {
-          rightToLeft: true,
-          text: this.el.nativeElement.dataset.text,
-          chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-          speed: 0.3,
-          revealDelay: 0.5
-        },
+          tweenLength: false,
+          text: this.text(),
+          chars: this.chars(),
+          revealDelay: this.revealDelay(),
+          speed: this.speed(),
+          delimiter: this.delimiter(),
+        }
       });
     }
   }
-  
 }

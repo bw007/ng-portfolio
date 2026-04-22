1ed86e4ea3cfd6b77c81ef588cc23294c83f0368
bw007 <mr.khasanov02@gmail.com>
2026-04-22 23:57:15 +0500

    feat: add new hero component

--- src/app/feature/home/hero/hero.component.ts
+++ src/app/feature/home/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { ScrambleTextDirective } from "@app/shared/directives/scramble-text.directive";
+import { ScrambleTextDirective } from "@shared/directives/scramble-text.directive";
+import { ButtonComponent } from "@shared/ui/button.component";
 
 @Component({
   selector: 'app-hero',
-  imports: [ScrambleTextDirective],
+  imports: [ScrambleTextDirective, ButtonComponent],
   templateUrl: './hero.component.html',
 })
 export class HeroComponent {}

276b537d0ac0f3d14a6b9f2a762605797ccccdfa
bw007 <mr.khasanov02@gmail.com>
2026-04-23 18:55:54 +0500

    feat: update header component with new navigation structure and button integration

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
+import { ButtonComponent } from "@app/shared/ui/button/button.component";
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  imports: [ButtonComponent, RouterLink],
 })
 export class HeaderComponent {
   isMobileMenuOpen = signal(false);

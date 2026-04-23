b6a4f83b77e41d0b2887d43b7237e62c78016575
bw007 <mr.khasanov02@gmail.com>
2026-04-23 23:44:49 +0500

    fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { LayoutComponent } from './layout/layout.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LayoutComponent],
-  template: `<app-layout />`
+  imports: [RouterOutlet],
+  template: `<router-outlet />`
 })
 export class App {}

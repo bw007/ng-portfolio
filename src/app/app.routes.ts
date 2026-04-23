e35c97b7c7fbaac5be1498a2d00bee2dcbe1d60d
bw007 <mr.khasanov02@gmail.com>
2026-04-23 23:44:37 +0500

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,7 +13,23 @@ export const routes: Routes = [
       },
       {
         path: 'home',
+        title: 'Home | NgPortfolio',
         loadComponent: () => import('./feature/home/home.component').then(m => m.HomeComponent),
+      },
+      {
+        path: 'about',
+        title: 'About | NgPortfolio',
+        loadComponent: () => import('./feature/about/about.component').then(m => m.AboutComponent),
+      },
+      {
+        path: 'projects',
+        title: 'Projects | NgPortfolio',
+        loadComponent: () => import('./feature/projects/projects.component').then(m => m.ProjectsComponent),
+      },
+      {
+        path: 'contact',
+        title: 'Contact | NgPortfolio',
+        loadComponent: () => import('./feature/contact/contact.component').then(m => m.ContactComponent),
       },
     ],
   },

8bae6f2c5ff06523b78d1c4e099c58783730c2e8
bw007 <mr.khasanov02@gmail.com>
2026-04-23 18:56:21 +0500

    feat: add button types for severity, type, and size

---
+++ src/app/shared/ui/button/button.types.ts
@@ -0,0 +1,3 @@
+export type ButtonSeverity = 'success' | 'info' | 'warning' | 'danger' | 'angular' | 'text';
+export type ButtonType = 'button' | 'submit' | 'reset';
+export type ButtonSize = 'small' | 'medium' | 'large';

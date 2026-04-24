88c8a58de1b055d1a34e7559ff7b5498b7d3e2cb
bw007 <mr.khasanov02@gmail.com>
2026-04-24 23:52:58 +0500

    feat: update hero section with personalized introduction and improved routing for buttons

--- src/app/feature/home/hero/hero.component.ts
+++ src/app/feature/home/hero/hero.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from "@shared/ui/button/button.component";
 import { IconComponent } from "@shared/ui/icon/icon.component";
 import { IconName } from '@shared/ui/icon/icon.types';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-hero',
-  imports: [ButtonComponent, IconComponent],
+  imports: [ButtonComponent, IconComponent, RouterLink],
   templateUrl: './hero.component.html',
 })
 export class HeroComponent {
   readonly socialLinks: { name: string; href: string; icon: IconName }[] = [
-    { name: 'Email', href: 'mailto:[EMAIL_ADDRESS]', icon: 'mail' },
-    { name: 'Telegram', href: 'https://t.me/l_hasanov', icon: 'send' },
+    { name: 'Telegram', href: 'https://t.me/hasanov_l', icon: 'send' },
     { name: 'LinkedIn', href: 'https://www.linkedin.com/in/lazizbek-hasanov-600374311/', icon: 'linkedin' },
     { name: 'GitHub', href: 'https://github.com/bw007', icon: 'github' },
   ];

fd7b8cdee9ad0a7b06f71d4ae1482d3662964a4e
bw007 <mr.khasanov02@gmail.com>
2026-04-27 23:43:18 +0500

    feat: create Not Found component

---
+++ src/app/feature/not-found/not-found.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '@app/shared/ui/button/button.component';
+
+@Component({
+  selector: 'app-not-found',
+  imports: [ButtonComponent],
+  templateUrl: './not-found.component.html',
+})
+export class NotFoundComponent {}

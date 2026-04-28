6860e53463c6f81a0eaa9dd0dce4d17a802ff4e7
bw007 <mr.khasanov02@gmail.com>
2026-04-28 23:58:25 +0500

    fix: update contact component to include icon and button imports, and rename send method to sendEmail

--- src/app/feature/contact/contact.component.ts
+++ src/app/feature/contact/contact.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
+import { IconComponent } from "@app/shared/ui/icon/icon.component";
+import { ButtonComponent } from "@app/shared/ui/button/button.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, IconComponent, ButtonComponent],
   templateUrl: './contact.component.html',
 })
 export class ContactComponent {
@@ -18,7 +20,7 @@ export class ContactComponent {
   error = signal<string | null>(null);
   sent = signal(false);
 
-  async send() {
+  async sendEmail() {
     if(this.form.invalid) {
       this.form.markAllAsTouched();
       return;
@@ -27,10 +29,32 @@ export class ContactComponent {
     this.error.set(null);
     this.sent.set(false);
 
+    const templateParams = {
+      from_name: this.form.value.name,
+      message: this.form.value.message,
+      reply_to: this.form.value.email
+    };
+    
     try {
+      const res: EmailJSResponseStatus = await emailjs.send(
+        process.env.NG_APP_GMAIL_SERVICE_ID,
+        process.env.NG_APP_GMAIL_TEMPLATE_ID,
+        templateParams,
+        { publicKey: process.env.NG_APP_GMAIL_SERVICE_PUBLIC_KEY }
+      );
+      console.log(res);
       
+      if (res.status === 200) {
+        this.sent.set(true);
+        this.form.reset();
+      };
+
     } catch (error) {
-      
+      const err = error as EmailJSResponseStatus;
+      this.error.set(err.text || 'Something went wrong. Please try again later');
+      console.error('EmailJS Error:', err);
+    } finally {
+      this.sending.set(false);
     }
   }
 }
